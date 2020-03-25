b957cbae1613a96268fb67ee0bf2ef38b1df572a
Emeline G. <emeline.giordano@orange.fr>
2020-03-25 14:14:46 +0100

    Ajout d'erreur Article dans codesResultatBLL Modif articleManager pour insert

--- src/fr/eni/serdaigle/bll/ArticleManager.java
+++ src/fr/eni/serdaigle/bll/ArticleManager.java
@@ -2,36 +2,84 @@ package fr.eni.serdaigle.bll;
 
 import fr.eni.serdaigle.dal.DAOFactory;
 import fr.eni.serdaigle.exception.BusinessException;
+
+import java.time.LocalDate;
+
 import fr.eni.serdaigle.bo.ArticleVendu;
 import fr.eni.serdaigle.dal.ArticleDAO;
 
 public class ArticleManager {
 	private ArticleDAO articleDAO;
+	private static final int CHAMPS_VARCHAR_30 = 30;
+	private static final int CHAMPS_VARCHAR_300 = 300;
 	
 
 	public ArticleManager() {
 		articleDAO = DAOFactory.getArticleDAO();
 	}
 
-	public ArticleVendu ajouterArticle(String pseudo, String nom, String prenom, String email, String telephone,
-			String rue, String codePostal, String ville, String motDePasse)
+	public ArticleVendu ajouterArticle(String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, int prixInitial, int prixVente, int noUtilisateur, int noCategorie)
 			throws BusinessException {
 		BusinessException be = new BusinessException();
-		// valider les champs
 
+//valider les champs
+		validerNomArticle(nomArticle, CHAMPS_VARCHAR_30, be);
+		validerDescriptionArticle(description, CHAMPS_VARCHAR_300, be);
+		validerDateEncheres(dateDebutEncheres, dateFinEncheres, be);
+		validerPrix(prixInitial, be);
 
 		ArticleVendu article = null;
-
+		
 		if (!be.hasErreurs()) {
 			article = new ArticleVendu();
-			// faire les sets
-			
 
+			article.setNomArticle(nomArticle);
+			article.setDescription(description);
+			article.setDateDebutEncheres(dateDebutEncheres);
+			article.setDateFinEncheres(dateFinEncheres);
+			article.setPrixInitial(prixInitial);
+			article.setPrixVente(article.getPrixInitial());
+			article.setNoUtilisateur(noUtilisateur);
+			article.setNoCategorie(noCategorie);
+			
 			articleDAO.insert(article);
 		} else {
 			throw be;
 		}
 		return article;
+	}
+	
+	public void validerNomArticle(String nomArticle, int varchar, BusinessException be) {
+		if (nomArticle == null || nomArticle.equals("")) {
+			be.ajouterErreur(CodesResultatBLL.CHAMP_NOM_ARTICLE_OBLIGATOIRE);
+		}
+		if (nomArticle.length() > varchar) {
+			be.ajouterErreur(CodesResultatBLL.CHAMP_NOM_ARTICLE_TAILLE_DEPASSEE);
+		}
+
+	}
+	
+	public void validerDescriptionArticle(String description, int varchar, BusinessException be) {
+		if (description == null || description.equals("")) {
+			be.ajouterErreur(CodesResultatBLL.CHAMP_DESCRIPTION_ARTICLE_OBLIGATOIRE);
+		}
+		if (description.length() > varchar) {
+			be.ajouterErreur(CodesResultatBLL.CHAMP_DESCRIPTION_ARTICLE_TAILLE_DEPASSEE);
+		}
+
+	}
+	
+	private void validerDateEncheres(LocalDate dateDebutEncheres, LocalDate dateFinEncheres, BusinessException be) {
+		if(dateDebutEncheres == null || dateFinEncheres == null || dateDebutEncheres.isAfter(dateFinEncheres) ) {
+			be.ajouterErreur(CodesResultatBLL.DATE_ERREUR);
+		}
+	}
+	
+	public void validerPrix(int prixInitial, BusinessException be) {
+		if (prixInitial == 0) {
+			be.ajouterErreur(CodesResultatBLL.PRIX_ERREUR);
+		}
 	}
 
 }
--- src/fr/eni/serdaigle/bll/CodesResultatBLL.java
+++ src/fr/eni/serdaigle/bll/CodesResultatBLL.java
@@ -8,5 +8,16 @@ public abstract class CodesResultatBLL {
 	
 	public static final int CHAMP_OBLIGATOIRE = 20000;
 	public static final int CHAMP_TAILLE_DEPASSEE  = 20001;
-	public static final int MOT_DE_PASSE_DIFFERENT  = 20007;
+	public static final int CHAMP_NOM_ARTICLE_OBLIGATOIRE= 20002;
+	public static final int CHAMP_NOM_ARTICLE_TAILLE_DEPASSEE= 20003;
+	public static final int CHAMP_DESCRIPTION_ARTICLE_OBLIGATOIRE = 20004;
+	public static final int CHAMP_DESCRIPTION_ARTICLE_TAILLE_DEPASSEE = 20005;
+	public static final int DATE_ERREUR = 20006;
+	public static final int PRIX_ERREUR = 20007;
+	public static final int MOT_DE_PASSE_DIFFERENT  = 20008;
+	
+	
+	
+	
+	
 }
