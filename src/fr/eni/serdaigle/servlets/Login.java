0f9fb9f4b840dde7a8ab12911214c7359e97a9eb
Maxldev <maximeleraydev@gmail.com>
2020-03-30 12:12:40 +0200

    Rectification erreurs dues au merge

--- src/fr/eni/serdaigle/servlets/Login.java
+++ src/fr/eni/serdaigle/servlets/Login.java
@@ -3,7 +3,6 @@ package fr.eni.serdaigle.servlets;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
