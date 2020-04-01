04c17b1a2f3818400d5adf4d5129e9744b2a12ff
Emeline G. <emeline.giordano@orange.fr>
2020-04-01 15:33:05 +0200

    Rajout u.email dans sous requête de requête select enchereDAOJdbcImpl

--- src/fr/eni/serdaigle/dal/jdbc/EnchereDAOJdbcImpl.java
+++ src/fr/eni/serdaigle/dal/jdbc/EnchereDAOJdbcImpl.java
@@ -44,7 +44,7 @@ public class EnchereDAOJdbcImpl implements EnchereDAO{
 			"		    FROM ENCHERES e \r\n" + 
 			"			JOIN ARTICLES_VENDUS av ON e.no_article = av.no_article\r\n" + 
 			"			JOIN UTILISATEURS u ON u.no_utilisateur = e.no_utilisateur\r\n" + 
-			"		  GROUP BY 	av.no_article,u.pseudo,u.no_utilisateur)\r\n" + 
+			"		  GROUP BY 	av.no_article,u.pseudo,u.no_utilisateur, u.email)\r\n" + 
 			"	vme ON vme.no_article = av.no_article\r\n" + 
 			"	WHERE av.no_article = ? ORDER BY vme.enchere_max DESC;";
 	private static final String SELECT_ALL_ENCHERES_EN_COURS = "SELECT DISTINCT a.nom_article, a.date_fin_encheres, a.prix_initial, u.pseudo, r.rue, r.ville, vme.val_max FROM ARTICLES_VENDUS a JOIN UTILISATEURS u ON u.no_utilisateur = a.no_vendeur LEFT JOIN ENCHERES e ON e.no_article = a.no_article LEFT JOIN RETRAITS r ON a.no_article = r.no_article LEFT JOIN (SELECT MAX(e.montant_enchere) as val_max, av.no_article FROM ENCHERES e JOIN ARTICLES_VENDUS av ON e.no_article = av.no_article GROUP BY av.no_article) vme ON vme.no_article = a.no_article WHERE a.date_debut_encheres < GETDATE();";
